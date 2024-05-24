16b1ba646c5beb12a2934badb622bca8e9ce8a27
Vladimir Osipov <osipov_VY@directum.ru>
2024-05-24 12:45:06 +0400

    Заполнение конфигов через конфиг и wcf в конфигах

---
+++ src/ManageAppProjects/map_plugin/init_util.ps1
@@ -0,0 +1,131 @@
+# Check config path
+$util_root = -join ($PSScriptRoot, '\..\..\..')
+$cfg_path = -join ($util_root, '\settings.yml')
+if (!(Test-Path $cfg_path))
+{
+    Write-Error (-join ('Config not found, path: ', $cfg_path))
+    return
+}
+
+function Create-Folders-If-Not-Exist
+{
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$TargetPath
+    )
+    if (!(Test-Path -Path $TargetPath))
+    {
+        New-Item -ItemType directory -Path $TargetPath
+    }
+}
+
+function Copy-Modified-Config
+{
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$SourcePath,
+        [Parameter(Mandatory = $true)]
+        [string]$TargetPath
+    )
+    $original_cfg = Get-Content -Path $SourcePath -Encoding 'utf8'
+    foreach ($line in $original_cfg)
+    {
+        if ( $line.StartsWith("    home_path:"))
+        {
+            $home_path = -join ($root_path, '\', $cfg.install_instance_name)
+            Add-Content -Path $TargetPath -Value "    home_path: $home_path" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("    home_path_src:"))
+        {
+            $home_path_src = -join ($root_path, '\', $cfg.install_instance_name, '\repo')
+            Add-Content -Path $TargetPath -Value "    home_path_src: $home_path_src" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("    database:"))
+        {
+            Add-Content -Path $TargetPath -Value "    database: '$( $cfg.install_instance_name )'" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("    DATABASE_ENGINE:"))
+        {
+            if (($cfg.db_engine -eq 'mssql') -or ($cfg.db_engine -eq 'postgres'))
+            {
+                Add-Content -Path $TargetPath -Value "    DATABASE_ENGINE: '$( $cfg.db_engine )'" -Encoding 'utf8'
+            }
+        }
+        elseif ($line.StartsWith("    CONNECTION_STRING:"))
+        {
+            if ($cfg.db_engine -eq 'mssql')
+            {
+                $connection_string = "data source=$( $cfg.db_server );initial catalog=$( $cfg.install_instance_name );user id=$( $cfg.db_user );Password=$( $cfg.db_password )"
+            }
+            elseif ($cfg.db_engine -eq 'postgres')
+            {
+                $connection_string = "server=$( $cfg.db_server );port=$( $cfg.postgres_port );database=$( $cfg.install_instance_name );user id=$( $cfg.db_user );Password=$( $cfg.db_password )"
+            }
+            else
+            {
+                Write-Error 'Wrong db_engine configuration'
+            }
+            Add-Content -Path $TargetPath -Value "    CONNECTION_STRING: '$connection_string'" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("    QUEUE_CONNECTION_STRING:"))
+        {
+            $queue_string = "virtualhost=$( $cfg.queue_virtual_host );hostname=$( $cfg.queue_server );port=5672;username=$( $cfg.queue_user );password=$( $cfg.queue_user );Exchange=$( $cfg.install_instance_name )"
+            Add-Content -Path $TargetPath -Value "    QUEUE_CONNECTION_STRING: '$queue_string'" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("    DATA_PROTECTION_CERTIFICATE_THUMBPRINT:"))
+        {
+            Add-Content -Path $TargetPath -Value "    DATA_PROTECTION_CERTIFICATE_THUMBPRINT: '$( $cfg.ssl_cert_thumbprint )'" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("    MONGODB_CONNECTION_STRING:"))
+        {
+            Add-Content -Path $TargetPath -Value "    MONGODB_CONNECTION_STRING: 'mongodb://$( $cfg.mongo_user ):$( $cfg.mongo_password )@$( $mongo_server ):27017'" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("    AUTHENTICATION_PASSWORD:"))
+        {
+            Add-Content -Path $TargetPath -Value "    AUTHENTICATION_PASSWORD: '$( $cfg.service_user_password )'" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("        COMPANY_CODE:"))
+        {
+            Add-Content -Path $TargetPath -Value "        COMPANY_CODE: '$( $cfg.company_code )'" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("    postgresql_bin:"))
+        {
+            Add-Content -Path $TargetPath -Value "    postgresql_bin: '$( $cfg.postgresql_bin )'" -Encoding 'utf8'
+        }
+        elseif ($line.StartsWith("    LOGS_PATH:"))
+        {
+            $root_logs_path = -join ($cfg.root_rx_path, '\', $cfg.logs_path_relative)
+            Add-Content -Path $TargetPath -Value "    LOGS_PATH: '$root_logs_path\{{ instance_name }}'" -Encoding 'utf8'
+        }
+        else
+        {
+            Add-Content -Path $TargetPath -Value $line -Encoding 'utf8'
+        }
+    }
+}
+
+
+# Parse cfg
+$cfg = Get-Content $cfg_path -Encoding 'utf8' | Out-String | ConvertFrom-Yaml
+$root_path = $cfg.root_rx_path
+$before_install_cfg_path = -join ($root_path, '\', $cfg.update_config_before_install_path_relative)
+$after_install_cfg_path = -join ($root_path, '\', $cfg.update_config_after_install_path_relative)
+$root_logs_path = -join ($root_path, '\', $cfg.logs_path_relative)
+
+# Create packages
+Create-Folders-If-Not-Exist -TargetPath (-join ($root_path, '\', $cfg.instances_path_relative)) | Out-Null
+Create-Folders-If-Not-Exist -TargetPath $root_logs_path | Out-Null
+
+# Modify install-cfgs and copy to target
+# TODO drop if error?
+if (Test-Path -Path $before_install_cfg_path)
+{
+    Remove-Item $before_install_cfg_path -Force | Out-Null
+}
+Copy-Modified-Config -SourcePath ( -join ($util_root, '\', 'update_config_before_install.yml')) -TargetPath $before_install_cfg_path | Out-Null
+
+if (Test-Path -Path $after_install_cfg_path)
+{
+    Remove-Item $after_install_cfg_path -Force | Out-Null
+}
+Copy-Modified-Config -SourcePath ( -join ($util_root, '\', 'update_config_after_install.yml')) -TargetPath $after_install_cfg_path | Out-Null
---
+++ src/ManageAppProjects/map_plugin/install_instance_simple.ps1
@@ -0,0 +1,60 @@
+Param ([string] $rx_instaler_dir_path, #РєР°С‚Р°Р»РѕРі СЃ РґРёСЃС‚СЂРёР±СѓС‚РёРІРѕРј СѓСЃС‚Р°РЅР°РІР»РёРІР°РµРјРѕРіРѕ Р±РёР»РґР°
+    [string] $instance_name, #РёРјСЏ РёРЅСЃС‚Р°РЅСЃР°
+    [int] $port                        #port
+)
+
+# Check config path
+$util_root = -join ($PSScriptRoot, '\..\..\..')
+$cfg_path = -join ($util_root, '\settings.yml')
+if (!(Test-Path $cfg_path))
+{
+    Write-Error (-join ('Config not found, path: ', $cfg_path))
+    return
+}
+
+$cfg = Get-Content $cfg_path -Encoding 'utf8' | Out-String | ConvertFrom-Yaml
+$root_path = $cfg.root_rx_path
+$instance_root_dir_path = (-join ($root_path, '\', $cfg.instances_path_relative))
+$map_plugin_path = $PSScriptRoot
+$cfg_before_install_path = -join ($root_path, '\', $cfg.update_config_before_install_path_relative)
+$cfg_after_install_path = -join ($root_path, '\', $cfg.update_config_after_install_path_relative)
+
+$script = "$PSScriptRoot/install_instance.ps1"
+
+$command = -join ("$script -rx_instaler_dir_path '$rx_instaler_dir_path' -instance_name $instance_name -port $port",
+"-instance_root_dir_path '$instance_root_dir_path' -map_plugin_path '$map_plugin_path'",
+"-cfg_before_install_path '$cfg_before_install_path'",
+"-cfg_after_install_path '$cfg_after_install_path'"
+)
+
+$params = @{
+    rx_instaler_dir_path = $rx_instaler_dir_path
+    instance_name = $instance_name
+    port = $port
+    instance_root_dir_path = $instance_root_dir_path
+    map_plugin_path = $map_plugin_path
+    cfg_before_install_path = $cfg_before_install_path
+    cfg_after_install_path = $cfg_after_install_path
+}
+
+& "$PSScriptRoot/install_instance.ps1" @params
+
+# HACK remove fake-comments
+$original_cfg_path = "$instance_root_dir_path\$instance_name\etc\config.yml"
+$original_cfg = Get-Content -Path $original_cfg_path -Encoding 'utf8'
+$tmp_cfg_path = "$original_cfg_path.tmp.yml"
+foreach ($line in $original_cfg)
+{
+    if ($line.Contains("#:auto_remove_comment:"))
+    {
+        $modified_line = $line -replace "#:auto_remove_comment:"
+        Add-Content -Path $tmp_cfg_path -Value $modified_line -Encoding 'utf8'
+    }
+    else
+    {
+        Add-Content -Path $tmp_cfg_path -Value $line -Encoding 'utf8'
+    }
+}
+
+Copy-Item -Path $tmp_cfg_path -Destination $original_cfg_path -Force
+Remove-Item -Path $tmp_cfg_path
